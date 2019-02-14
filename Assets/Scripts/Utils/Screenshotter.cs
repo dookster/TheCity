688ade1448ec0c9890f9b20c2872b41d4c4529ab
Aergia <dook@aergia.dk>
2019-02-14 22:43:30 +0100

    First commit of project. Plugins DOTween and DoodleStudio95 ignored

---
+++ Assets/Scripts/Utils/Screenshotter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshotter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshot" + System.DateTime.Now.ToString("yyyyMMddhhmmss") + ".jpg");
+        }
+    }
+}
