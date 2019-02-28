8b5613516091f4dcac7742126e204f96eed11f87
Aergia <dook@aergia.dk>
2019-02-28 01:49:09 +0100

    Added book and more prop sprites

--- Assets/Scripts/Characters/FacePlayer.cs
+++ Assets/Scripts/Characters/FacePlayer.cs
@@ -7,6 +7,12 @@ public class FacePlayer : MonoBehaviour
 
     public Transform player;
 
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/UI/InfoUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InfoUI : MonoBehaviour
+{
+
+    public GameObject infoParent;
+    public GameObject bookParent;
+    public TMP_InputField inputText;
+
+    public GameObject handsParent;
+    public FirstPersonDrifter playerDrifter;
+    public MouseLook mouseLook;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F1))
+        {
+            infoParent.SetActive(!infoParent.activeSelf);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if (bookParent.activeSelf)
+            {
+                // hide book
+                bookParent.SetActive(false);
+                playerDrifter.enabled = true;
+                handsParent.SetActive(true);
+                mouseLook.enabled = true;
+            }
+            else
+            {
+                // show book
+                bookParent.SetActive(true);
+                inputText.Select();
+                inputText.ActivateInputField();
+                inputText.text = "";
+                playerDrifter.enabled = false;
+                handsParent.SetActive(false);
+                mouseLook.enabled = false;
+            }
+        }
+    }
+}
